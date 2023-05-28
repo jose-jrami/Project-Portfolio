6e9ed408cae6226d978ede763e60ee16f3d37980
jose-jrami <134811621+jose-jrami@users.noreply.github.com>
2023-05-28 01:28:58 -0700

    Added more content to project pages

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/project1')
 def project1():
